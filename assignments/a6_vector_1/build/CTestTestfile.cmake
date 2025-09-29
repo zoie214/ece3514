1ee4cde7080e8bd341b2c337b8c8204f864f4c83
zoie214 <zmill448@icloud.com>
2025-09-29 19:44:20 -0400

    final

---
+++ assignments/a6_vector_1/build/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/zoiem/vector_project/ece3514/assignments/a6_vector_1
+# Build directory: C:/Users/zoiem/vector_project/ece3514/assignments/a6_vector_1/build
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(my_test "C:/Users/zoiem/vector_project/ece3514/assignments/a6_vector_1/build/my_test.exe")
+set_tests_properties(my_test PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/zoiem/vector_project/ece3514/assignments/a6_vector_1/CMakeLists.txt;20;add_test;C:/Users/zoiem/vector_project/ece3514/assignments/a6_vector_1/CMakeLists.txt;0;")
