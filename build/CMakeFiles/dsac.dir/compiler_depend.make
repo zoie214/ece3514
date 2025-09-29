0b89c2c98373da8a274763cbc9bcfe3dc04e953e
zoie214 <zmill448@icloud.com>
2025-09-29 18:25:33 -0400

    initial commit

---
+++ build/CMakeFiles/dsac.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for dsac.
+# This may be replaced when dependencies are built.
---
+++ build/CMakeFiles/my_test.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/my_test.dir/tests/test_vector.cpp.obj"
+  "CMakeFiles/my_test.dir/tests/test_vector.cpp.obj.d"
+  "libmy_test.dll.a"
+  "my_test.exe"
+  "my_test.exe.manifest"
+  "my_test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/my_test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
